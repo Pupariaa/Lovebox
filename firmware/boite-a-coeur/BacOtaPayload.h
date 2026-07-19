66ee07f658a9873f4e56b2d9943b63c2490e3508
Maxime <32194766+Pupariaa@users.noreply.github.com>
2026-07-19 04:37:27 +0200

    feat(firmware): update BacOtaPayload.h

--- firmware/boite-a-coeur/BacOtaPayload.h
+++ firmware/boite-a-coeur/BacOtaPayload.h
@@ -13,6 +13,7 @@ struct BacOtaPayload {
     char assetsUrl[256] = {};
     char assetsSha256[65] = {};
     uint32_t assetsSize = 0;
+    char assetsManifestUrl[256] = {};
 
     bool hasFirmware() const { return fwUrl[0] != 0 && fwSha256[0] != 0 && fwSize > 0; }
     bool hasAssets() const { return assetsUrl[0] != 0 && assetsSha256[0] != 0 && assetsSize > 0; }
@@ -31,6 +32,7 @@ struct BacOtaPayload {
         String fwSha = BacCloudClient::extractJsonString(json, "sha256");
         String assetsUrl = BacCloudClient::extractJsonString(json, "assets_url");
         String assetsSha = BacCloudClient::extractJsonString(json, "assets_sha256");
+        String assetsManifestUrl = BacCloudClient::extractJsonString(json, "assets_manifest_url");
         if (!version.length()) return false;
         copyField(out.version, sizeof(out.version), version);
         copyField(out.fwUrl, sizeof(out.fwUrl), fwUrl);
@@ -39,6 +41,7 @@ struct BacOtaPayload {
         copyField(out.assetsUrl, sizeof(out.assetsUrl), assetsUrl);
         copyField(out.assetsSha256, sizeof(out.assetsSha256), assetsSha);
         out.assetsSize = BacCloudClient::extractJsonUInt(json, "assets_size");
+        copyField(out.assetsManifestUrl, sizeof(out.assetsManifestUrl), assetsManifestUrl);
         return out.valid();
     }
 };
