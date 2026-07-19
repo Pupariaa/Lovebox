f52bfe0976a89c7b1c40c518dea1144dbaa2ecf6
Maxime <32194766+Pupariaa@users.noreply.github.com>
2026-07-19 10:56:11 +0200

    backup: applications/mobile-rn/src/app/box-settings.tsx

--- applications/mobile-rn/src/app/box-settings.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from "expo-router";
-import { useAppStore } from "@/store/appStore";
-
-export default function BoxSettingsScreen() {
-  const myDevice = useAppStore((s) => s.myDevice);
-  if (myDevice) return <Redirect href={`/device/${myDevice.id}`} />;
-  return <Redirect href="/boxes" />;
-}
