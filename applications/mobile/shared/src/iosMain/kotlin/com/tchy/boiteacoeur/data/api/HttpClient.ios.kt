1f01c98e0c3456c61b88d8141874f43e8a345aca
Maxime <32194766+Pupariaa@users.noreply.github.com>
2026-07-19 10:56:17 +0200

    backup: applications/mobile/shared/src/iosMain/kotlin/com/tchy/boiteacoeur/data/api/HttpClient.ios.kt

--- applications/mobile/shared/src/iosMain/kotlin/com/tchy/boiteacoeur/data/api/HttpClient.ios.kt
+++ applications/mobile/shared/src/iosMain/kotlin/com/tchy/boiteacoeur/data/api/HttpClient.ios.kt
@@ -2,5 +2,16 @@ package com.tchy.boiteacoeur.data.api
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.darwin.Darwin
+import io.ktor.client.plugins.HttpTimeout
 
-actual fun createHttpClient(): HttpClient = HttpClient(Darwin)
+actual fun createHttpClient(): HttpClient = HttpClient(Darwin) {
+    install(HttpTimeout) {
+        connectTimeoutMillis = HTTP_CONNECT_TIMEOUT_MS
+        socketTimeoutMillis = HTTP_SOCKET_TIMEOUT_MS
+        requestTimeoutMillis = HTTP_REQUEST_TIMEOUT_MS
+    }
+}
+
+private const val HTTP_CONNECT_TIMEOUT_MS = 15_000L
+private const val HTTP_SOCKET_TIMEOUT_MS = 30_000L
+private const val HTTP_REQUEST_TIMEOUT_MS = 30_000L
