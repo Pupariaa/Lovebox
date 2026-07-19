54a045eda1cbd5f878658cdf680a2e269fc09f6c
Maxime <32194766+Pupariaa@users.noreply.github.com>
2026-07-19 10:56:14 +0200

    backup: applications/mobile/shared/src/androidMain/kotlin/com/tchy/boiteacoeur/data/api/HttpClient.android.kt

--- applications/mobile/shared/src/androidMain/kotlin/com/tchy/boiteacoeur/data/api/HttpClient.android.kt
+++ applications/mobile/shared/src/androidMain/kotlin/com/tchy/boiteacoeur/data/api/HttpClient.android.kt
@@ -2,11 +2,21 @@ package com.tchy.boiteacoeur.data.api
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.HttpTimeout
 
 actual fun createHttpClient(): HttpClient = HttpClient(OkHttp) {
+    install(HttpTimeout) {
+        connectTimeoutMillis = HTTP_CONNECT_TIMEOUT_MS
+        socketTimeoutMillis = HTTP_SOCKET_TIMEOUT_MS
+        requestTimeoutMillis = HTTP_REQUEST_TIMEOUT_MS
+    }
     engine {
         config {
             retryOnConnectionFailure(true)
         }
     }
 }
+
+private const val HTTP_CONNECT_TIMEOUT_MS = 15_000L
+private const val HTTP_SOCKET_TIMEOUT_MS = 30_000L
+private const val HTTP_REQUEST_TIMEOUT_MS = 30_000L
