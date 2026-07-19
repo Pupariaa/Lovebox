c7bd7f9a727ad5609092d094dbcf7e2d82ed51c2
Maxime <32194766+Pupariaa@users.noreply.github.com>
2026-07-19 04:34:29 +0200

    feat(web): add legal configuration

---
+++ web/backend/config/legal.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'publisher_name' => 'Techalchemy',
+    'publisher_form_fr' => 'Entrepreneur individuel (EI)',
+    'director' => 'ZORLONI Maxime',
+    'address' => '31 Avenue du clos banderet, 74200 Thonon-les-Bains, France',
+    'siret' => '97746385000015',
+    'rcs' => '977 463 850 R.C.S. Tarascon',
+    'vat' => 'FR82977463850',
+    'phone' => '+33 7 67 73 04 06',
+    'email' => 'support@boite-a-coeur.fr',
+    'legal_email' => 'contact@techalchemy.fr',
+    'site' => 'boite-a-coeur.fr',
+    'product' => 'Boîte à Cœur',
+    'data_retention_months' => 24,
+    'host_name' => 'PulseHeberg SAS',
+    'host_address' => '9 Boulevard de Strasbourg, 83000 Toulon, France',
+    'host_rcs' => '824 070 619 R.C.S. Toulon',
+    'host_siret' => '824 070 619 00039',
+    'host_phone' => '+33 4 22 14 13 60',
+    'mediation_url' => 'https://www.cnpm-mediation-consommation.eu/',
+    'mediation_name' => 'CNPM (Centre de la Médiation de la Consommation de conciliateurs de justice)',
+    'odr_url' => 'https://ec.europa.eu/consumers/odr',
+    'updated' => 'juillet 2026',
+];
